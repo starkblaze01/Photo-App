f54e265679bbe98e346b58734dce9cb0d754d15b
Mayank Pathela <mp.pathela@gmail.com>
2021-01-11 21:22:27 +0530

    Add groupBy date in View Component

--- client/src/components/View.js
+++ client/src/components/View.js
@@ -1,12 +1,52 @@
-import {useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import useFireStoreDoc from '../utils/useFireStoreDoc';
-import { Modal, Button } from 'antd';
+import { Modal, Button, Typography } from 'antd';
+
+const { Title } = Typography;
 
 function View() {
-    const { docs } = useFireStoreDoc('images');
-    console.log(docs);
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [index, setIndex] = useState(0);
+    const [lastDoc, setLastDoc] = useState(0);
+    const { groupBy, keys, totalDocs, docs } = useFireStoreDoc('images', lastDoc);
+    console.log(groupBy, keys, totalDocs, docs)
+    const [postList, setPostList] = useState({
+        list: [1, 2, 3, 4]
+    });
+    const loader = useRef(null);
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        var options = {
+            root: null,
+            rootMargin: "20px",
+            threshold: 1.0
+        };
+        // initialize IntersectionObserver
+        // and attaching to Load More div
+        const observer = new IntersectionObserver(handleObserver, options);
+        if (loader.current) {
+            observer.observe(loader.current)
+        }
+
+    }, []);
+
+    useEffect(() => {
+        // here we simulate adding new posts to List
+        const newList = postList.list.concat([1, 1, 1, 1]);
+        setPostList({
+            list: newList
+        })
+    }, [page, postList])
+
+    // here we handle what happens when user scrolls to Load More div
+    // in this case we just update page variable
+    const handleObserver = (entities) => {
+        const target = entities[0];
+        if (target.isIntersecting) {
+            setPage((page) => page + 1)
+        }
+    }
 
     const showModal = () => {
         setIsModalVisible(true);
@@ -20,17 +60,30 @@ function View() {
     const footer = (
         <div>
             <Button type="ghost" onClick={() => setIndex(prev => prev - 1)} disabled={index <= 0}>Previous</Button>
-            <Button type="primary" onClick={() => setIndex(prev => prev + 1)} disabled={index >= docs.length-1}>Next</Button>
+            <Button type="primary" onClick={() => setIndex(prev => prev + 1)} disabled={index >= totalDocs-1}>Next</Button>
         </div>
     )
 
+    const grid = keys.map((el) =>
+                    <div>
+                       <Title level={3}>
+                            {new Date(groupBy[el][0].createdAt.seconds * 1000).toDateString()} | {groupBy[el].length} Images
+                        </Title>
+                    
+                        <div className="img-grid">
+                            {groupBy[el] && groupBy[el].map(doc => (
+                            <div className="img-wrap" key={doc.id}>
+                                <img src={doc.url} alt={doc.fileName} onClick={() => { showModal(); setIndex(docs.indexOf(doc)) }} style={{ cursor: 'pointer' }} />
+                            </div>
+                            ))}
+                        </div>
+
+                    </div>
+                )
+
     return (
-        <div className="img-grid">
-            {docs && docs.map(doc => (
-                <div className="img-wrap" key={doc.id}>
-                    <img src={doc.url} alt={doc.fileName} onClick={() => { showModal(); setIndex(docs.indexOf(doc))}}/>
-                </div>
-            ))}
+        <div>
+            {grid}
             { docs && docs.length ? <Modal title="Image Preview" closable={true} centered={true} 
                 onCancel={handleCancel}
                 visible={isModalVisible}
@@ -39,6 +92,9 @@ function View() {
                 bodyStyle={{height: '80vh'}}>
                 <img src={docs[index].url} alt={docs[index].fileName} style={{ width: '100%', height: '100%'}}/>
             </Modal> : ''}
+            <div className="loading" ref={loader}>
+                <h2>Load More</h2>
+            </div>
         </div>
     )
 }
--- client/src/utils/useFireStoreDoc.js
+++ client/src/utils/useFireStoreDoc.js
@@ -2,26 +2,52 @@ import { useState, useEffect } from 'react';
 import { store } from './firebase';
 
 
-const useFireStoreDoc = (collection) => {
+const useFireStoreDoc = (collection, last) => {
     const [docs, setDocs] = useState([]);
 
     useEffect(() => {
+        if (last){
+            const unsub = store.collection(collection)
+                .orderBy("createdAt", "desc")
+                .startAt(last)
+                .limit(30)
+                .onSnapshot((snap) => {
+                    let documents = []
+                    snap.forEach((doc) => {
+                        documents.push({ ...doc.data(), id: doc.id })
+                    });
+                    setDocs(documents)
+                });
+            return () => unsub();
 
-        const unsub = store.collection(collection)
-        .orderBy("createdAt", "desc")
-        .onSnapshot((snap) => {
-            let documents = []
-            snap.forEach((doc) => {
-                documents.push({...doc.data(), id: doc.id })
-            });
-            setDocs(documents)
-        });
+        } else {
+            const unsub = store.collection(collection)
+                .orderBy("createdAt", "desc")
+                .limit(30)
+                .onSnapshot((snap) => {
+                    let documents = []
+                    snap.forEach((doc) => {
+                        documents.push({ ...doc.data(), id: doc.id })
+                    });
+                    setDocs(documents)
+                });
+            return () => unsub();
 
-        return () => unsub();
-
-    }, [collection])
-
-    return { docs };
+        }
+    }, [collection, last]);
+    let groupBy = {}
+    docs.forEach((el) => {
+        const date = new Date(el.createdAt.seconds * 1000).getDate()
+        if (groupBy[date]) {
+            groupBy[date].push(el);
+        } else {
+            groupBy[date] = [el];
+        }
+    })
+    console.log(groupBy);
+    let keys = Object.keys(groupBy).reverse()
+    let totalDocs = docs.length
+    return { groupBy, keys, totalDocs, docs};
 }
 
 export default useFireStoreDoc;
